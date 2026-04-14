ffce18ac0e70ad4167f52028ca1b367a79f917e8
Kimi Qin <440041588@gapps.yrdsb.ca>
2026-04-14 12:02:22 -0400

    Reset banana

--- Banana.java
+++ Banana.java
@@ -5,6 +5,23 @@ public class Banana extends Actor
 {
     public void act()
     {
-        move(-2);
+        move(-10);
+        
+        if(getX() <= 0){
+            resetBanana();
+        }
+    }
+    
+    public void resetBanana()
+    {
+        int x = Greenfoot.getRandomNumber(2);
+        if(x == 0){
+            setLocation(600, 100);
+        }
+        else
+        {
+            setLocation(600, 300);
+        }
+    
     }
 }
